9f09d034ac9e0de5f7ebfd4ab76283945eb76954
3irsari <113298598+3irsari@users.noreply.github.com>
2025-01-13 23:36:49 +0100

    Update Open ended Question Extraction.py

--- Open ended Question Extraction.py
+++ Open ended Question Extraction.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Attempt to read the CSV file with a different encoding
-df = pd.read_csv("C:/Users/x/OneDrive - Berlin School of Business and Innovation (BSBI)/Desktop/BSBI/Dissertation/Data-Cleaning/survey_data.csv", encoding='latin1')
+df = pd.read_csv("*/survey_data.csv", encoding='latin1')
 
 # Select first 4 and last 3 columns
 selected_columns = df.iloc[:, list(range(4)) + list(range(-3, 0))]
@@ -17,6 +17,6 @@ row_count = len(df_cleaned)
 print(f"Number of rows in the cleaned DataFrame: {row_count}")
 
 # Save the cleaned DataFrame to a CSV file
-df_cleaned.to_csv("C:/Users/x/OneDrive - Berlin School of Business and Innovation (BSBI)/Desktop/BSBI/Dissertation/Data-Cleaning/Open_EndedQuestionAnswers.csv", index=False, encoding='utf-8')
+df_cleaned.to_csv("*/Open_EndedQuestionAnswers.csv", index=False, encoding='utf-8')
 
 print("New CSV file has been created with the selected columns, no null values, and fixed encoding.")
